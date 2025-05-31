c262ad4084dc84d2b9b88df936a144a268483809
Punit Turlapati <Punitsst@gmail.com>
2025-05-30 22:01:39 -0500

    Extract login logs from gz files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use flate2::read::GzDecoder;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+use std::process;
+
 fn main() {
-    println!("minecraft log-parser!");
+    let log_path = Path::new("log_export_2017");
+    let files = fs::read_dir(log_path).unwrap();
+
+    for read_file in files {
+        match read_file {
+            Ok(file) => {
+                let file = fs::File::open(file.path()).unwrap();
+                let mut gz = GzDecoder::new(file);
+                let mut string = String::new();
+
+                gz.read_to_string(&mut string).unwrap();
+
+                let lines = string.lines().filter(|line| line.contains("logged"));
+                for line in lines {
+                    println!("{}", line)
+                }
+            }
+            Err(_) => process::exit(0),
+        }
+    }
 }
